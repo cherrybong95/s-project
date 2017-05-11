cfe717d1d747f63323572c66d30225dad4e1695c
Administrator <Administrator@192.168.0.116>
2017-05-11 18:55:30 +0900

    김종봉 리스트 주석추가

--- semiproject/src/controller/FileUploadController.java
+++ semiproject/src/controller/FileUploadController.java
@@ -18,7 +18,7 @@ public class FileUploadController implements Controller {
 	       //String simple_info=fileManager.
 			//db에 파일의 정보와 이미지파일의 경로를 저장
 	    		   String pno=fileManager.addProduct();
-	    		   
+	    		   //상품 등록후 등록한 상품정보를 보여주기위해 상품번호를 url에 전달
 	       return  "redirect:DispatcherServlet?command=showProductDetail&productNo="+pno;
 	}
 
--- semiproject/src/controller/ListController.java
+++ semiproject/src/controller/ListController.java
@@ -8,7 +8,7 @@ import model.ListVO;
 import model.MakerDAO;
 import model.MockDAO;
 import model.PagingBean;
-
+//상품리스트를 메인화면에 출력하는 컨트롤러
 public class ListController implements Controller {
 
 	@Override
@@ -19,18 +19,24 @@ public class ListController implements Controller {
 		allMakerMember=MakerDAO.getInstance().countMakerMember();
 		allBuyerMember=BuyerDAO.getInstance().countBuyerMember();
 		System.out.println("리스트작업 시작");
-
+		//현재 페이지 정보를 얻는다
 		 nowPage=request.getParameter("pageNo");
 		
 		if(nowPage==null)
 			nowPage="1";
 			System.out.println(nowPage);
+			//상품리스트와 페이지 정보를 저장하는 ListVO 객체를 생성한다
 		ListVO listVO=new ListVO();
+		//페이징을 위한 총 상품갯수를 받아온다
 		int contentNo=MockDAO.getInstance().getContentNo();
+		//페이징을 위한 페이징빈 객체를 생성하고 생성자에 총 상품수와 현재페이지 정보를 전달한다
 		PagingBean pagingBean=new PagingBean(contentNo,Integer.parseInt(nowPage));
+		//DAO 로 부터 리스트반환 메소드를 실행시킨다 페이징정보를 전달하여
 		listVO=MockDAO.getInstance().getAllList(pagingBean);
+		//requestScope에 리스트와 페이지정보를 저장한다
 		request.setAttribute("productListVO", listVO);
 		//request.setAttribute("memberCountBuyer", arg1);
+		//requestScope에 판매자와 구매자 회원수를 저장한다
 		request.setAttribute("MakerMember", allMakerMember);
 		request.setAttribute("BuyerMember", allBuyerMember);
 		return "page/list.jsp";
