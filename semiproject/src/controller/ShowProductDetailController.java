f69dd7f5db1fc35c88c86513c33c917ec2a12a92
Administrator <Administrator@192.168.0.116>
2017-05-11 19:02:03 +0900

    김종봉 상품상세보기주석

--- semiproject/src/controller/ShowProductDetailController.java
+++ semiproject/src/controller/ShowProductDetailController.java
@@ -5,18 +5,22 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.MockDAO;
 import model.ProductVO;
-
+	//상품의 상세정보를 나타내는 컨트롤러
 public class ShowProductDetailController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String url="";
 		int productNo=Integer.parseInt(request.getParameter("productNo"));
+		//getProductDetail 메소드를 통해 상품의 정보를 가져온다
 		ProductVO pvo=MockDAO.getInstance().getProductDetail(productNo);
+		//상품정보가 없으면 에러페이지
 		if(pvo==null)
 			url="error.jsp";
+		//상품정보가 있으면 requestScope에 저장한다
 		else {
 			request.setAttribute("productDetail", pvo);
+			//상세정보를 표시하는 페이지로 이동시킨다
 			url="page/showProductDetail.jsp";
 		}
 		
