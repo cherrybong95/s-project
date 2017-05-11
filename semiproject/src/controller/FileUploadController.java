3a54b91f3ed4c8ee4260b43b7ababcf3661bf306
Administrator <Administrator@192.168.0.116>
2017-05-11 18:51:27 +0900

    김종봉 주석

--- semiproject/src/controller/FileUploadController.java
+++ semiproject/src/controller/FileUploadController.java
@@ -12,10 +12,11 @@ public class FileUploadController implements Controller {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 	
-		
+			//파일 업로드하는 객체를 생성하고 생성자에 request를 전달
 			FileManager fileManager = new FileManager(request);
 				
 	       //String simple_info=fileManager.
+			//db에 파일의 정보와 이미지파일의 경로를 저장
 	    		   String pno=fileManager.addProduct();
 	    		   
 	       return  "redirect:DispatcherServlet?command=showProductDetail&productNo="+pno;
